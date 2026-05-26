702dc63e7484cba8c22a485164b19fe7335838cd
Will Friel <williamfriel2003@gmail.com>
2026-05-26 12:37:32 -0400

    Working on comments

--- outing_report.py
+++ outing_report.py
@@ -1,3 +1,4 @@
+# Imports
 import streamlit as st
 import plotly.graph_objects as go
 import pandas as pd
@@ -6,8 +7,10 @@ import pickle
 import numpy as np
 MODELS_CACHE = {}
 
+# Makes it so app takes up full page
 st.set_page_config(layout="wide")
 
+# Allows scrolling on app
 st.markdown(
     """
     <style>
@@ -20,23 +23,29 @@ st.markdown(
     unsafe_allow_html=True
 )
 
+# File upload - allows for multiple csv files to be uploaded at once
 files = st.file_uploader("Import Trackman file", type="csv", accept_multiple_files=True)
 
+# If user clicks "Refresh" button, data resets. Essentially, any changes made in file upload will be updated -- File(s) added/removed will now be updated
 if st.button("Refresh"):
     del st.session_state.data
     st.session_state.file_key = st.session_state.get("file_key", 0) + 1 
     st.rerun()
 
+# Concatenates all csv files uploaded
 if files and "data" not in st.session_state:
     dfs = [pd.read_csv(f) for f in files]
     st.session_state.data = pd.concat(dfs, ignore_index=True)
 
+# Message for if there are no csv files uploaded
 if "data" not in st.session_state:
     st.info("Please upload a CSV to get started.")
     st.stop()
 
+# Assigns concatenated csv files to variable
 data = st.session_state.data
 
+# Remaps pitch type column to match what is used in our modelling (see 'stuff+' folder)
 data["TaggedPitchType"] = data["TaggedPitchType"].replace({
         "Fastball": "FA",
         "Slider": "SL",
@@ -49,46 +58,81 @@ data["TaggedPitchType"] = data["TaggedPitchType"].replace({
 
 ############## INTERACTIVE PITCH CHARTS
 
+# In this section we create and plot all 5 of the interactive pitch charts that appear on the page
+
+# This is an example of creating streamlit columns, which essentially allows us to control the size of everything that is produced relative to the width of the page
+# For a column of size x, that column will take up x / sum(cols) percent of the page
+# In this instance, we create 3 columns. The sizes (in this case [1,2,1]), creates columns of those proportions, whose sum is the width of the full page.
 x1,x2,x3 = st.columns([1,2,1])
+
+# Since we are using x2, we use a column that will print to 2/4, or half of the page
 with x2:
+    # Create a list of all unique pitchers in dataframe for users to select from
     names = list(data["Pitcher"].unique())
+
+    # Create selectbox where user can select pitcher they want to analyze
     pitcher = st.selectbox("Choose a Pitcher", options=names)
 
+# Once pitcher is selected, we filter the dataframe so ONLY that pitcher's pitches are included
 df = data[data["Pitcher"] == pitcher]
-df = df[df["TaggedPitchType"] != "Other"]
 
+# Filter df so it only includes pitches that we have models developed for
+pitchtypes = ["FA", "SL", "CH", "CU", "FC", "SI", "FS"]
+df = df[df["TaggedPitchType"].isin(pitchtypes)]
+
+# Creating column that converts Release Side/Height to inches, to be used for Release Point Chart
 df["RelSidei"] = df["RelSide"] * 12
 df["RelHeighti"] = df["RelHeight"] * 12
 
+# Creates three equal-sized columns for our top 3 charts
 c1, c2, c3 = st.columns([1,1,1])
 
 ###### PITCH BREAK CHART
 
-with c2:
-    
-    pitch_types = ["FA", "SL", "CU", "CH", "SI", "FC", "FS"]
-    df = df[df["TaggedPitchType"].isin(pitch_types)]
+# The first chart we develop will be our standard Pitch Break Chart, which will appear in the upper middle section of the pitch plots
 
+with c2:
+
+    # Maps colors to pitch types, so they are consistent across all plots
     colors = {"FA": "red", "SL": "blue", "CU": "green", "CH": "orange",
               "SI": "brown", "FC": "gray", "FS": "purple"}
 
+    # Initializes empty plot
     fig1 = go.Figure()
+
+    # Iterates over each pitch, grouped by pitch type, and performs the following
     for pitch, group in df.groupby("TaggedPitchType"):
+
+        # for each pitch type group, adds a new scatter plot layer to the figure with:
         fig1.add_trace(go.Scatter(
+
+            # Horizontal Break on x-axis, Induced Vertical Break on y-axis
             x=group["HorzBreak"],
             y=group["InducedVertBreak"],
             mode="markers",
+
+            # Labels each dot as their respective pitch type
             name=pitch,
+
+            # Styles each dot to size 8, and the color is based on correlating pitch type color that we mapped earlier
             marker=dict(size=8, color=colors.get(pitch, "black")),
+
+            # Attaches dataframe row indeces to each point, will be helpful for click interactions later on
             customdata=group.index.tolist(),
         ))
 
     fig1.update_layout(
+
+        # Add titles
         title="Pitch Break Chart",
         xaxis_title="Horizontal Break (in)",
         yaxis_title="Induced Vertical Break (in)",
+
+        # Adjust size to fit EMBEDDED APP ON WEBSITE
         width=550,
         height=550,
+
+        # Set axes
         xaxis=dict(range=[-30, 30], showgrid=False, zeroline=True, zerolinecolor="black", zerolinewidth=2),
         yaxis=dict(range=[-30, 30], showgrid=False, zeroline=True, zerolinecolor="black", zerolinewidth=2),
         plot_bgcolor="white",
@@ -104,47 +148,71 @@ with c2:
         ]
     )
 
+    # Show the plot on app
+    # Every plot has to have its own unique key, this one is "plot1"
     event1 = st.plotly_chart(fig1, on_select="rerun", key="plot1")
 
+    # For when point(s) on the plot are selected
     if event1 and event1.selection and event1.selection.points:
+        # The selected point(s)
         selected_indices = [int(pt["customdata"]) for pt in event1.selection.points]
-        
+
+        # Present pitch type options for reclassification
         st.markdown(f"**{len(selected_indices)} pitches selected**")
         
         new_type = st.selectbox("Reclassify all selected as:", pitch_types)
-        
+
+        # Giving users the option to change the pitch type of selected points, or delete them entirely
         col1, col2 = st.columns(2)
         with col1:
             if st.button("Apply Change"):
-                for idx in selected_indices:
+                for idx in selected_indices:\
+                    # Reassigns selected pitches to selected pitch type
                     st.session_state.data.at[idx, "TaggedPitchType"] = new_type
+                # This line essentially refreshes the dataset across the whole app, everything that is affected by this change will be updated
                 st.rerun()
         with col2:
             if st.button("Delete Selected"):
+                # Deletes selected pitches
                 st.session_state.data = st.session_state.data.drop(index=selected_indices).reset_index(drop=True)
                 st.rerun()
 
 ###### RELEASE POINT CHART
 
+# The next chart will be our Release Point Chart, which will appear in the upper left section of the pitch plots
+
 with c1:
 
+    # Initialize plot
     fig3 = go.Figure()
     for pitch, group in df.groupby("TaggedPitchType"):
         fig3.add_trace(go.Scatter(
+
+            # Release Side (inches) on x-axis, Release Height (inches) on y-axis
             x=group["RelSidei"],
             y=group["RelHeighti"],
             mode="markers",
+
+            # Label each dot as their respective pitch type
             name=pitch,
+
+            # Everything here is identical to previous plot
             marker=dict(size=8, color=colors.get(pitch, "black"), line=dict(color="white", width=0.5)),
             customdata=group.index.tolist(),
         ))
 
     fig3.update_layout(
+
+        # Add titles
         title="Release Point Chart",
         xaxis_title="Release Side (in)",
         yaxis_title="Release Height (in)",
+
+        # Set size
         width=550,
         height=550,
+
+        # Set axes range
         xaxis=dict(range=[-48, 48], showgrid=False, zeroline=True, zerolinecolor="black", zerolinewidth=2),
         yaxis=dict(range=[0, 96], showgrid=False, zeroline=True, zerolinecolor="black", zerolinewidth=2),
         plot_bgcolor="white",
@@ -160,11 +228,14 @@ with c1:
         ]
     )
 
+    # Show plot with key "rel_plot"
     event3 = st.plotly_chart(fig3, on_select="rerun", key="rel_plot")
 
+    # When pitches are selected
     if event3 and event3.selection and event3.selection.points:
         selected_indices = [int(pt["customdata"]) for pt in event3.selection.points]
-        
+
+        # Users can only delete pitches on this plot, not reassign their pitch type
         col1, col2 = st.columns(2)
         with col1:
             if st.button("Delete Selected"):
@@ -172,26 +243,41 @@ with c1:
                 st.rerun()
 
 ###### PITCH LOCATION CHART
+
+# The next chart will be our Pitch Location Chart, which will appear in the upper right section of the pitch plots
     
 with c3:
 
+    # Initialize plot
     fig4 = go.Figure()
     for pitch, group in df.groupby("TaggedPitchType"):
         fig4.add_trace(go.Scatter(
+
+            # Plate Location Side (ft) on x-axis, Plate Location Height (feet) on y-axis
             x=group["PlateLocSide"],
             y=group["PlateLocHeight"],
             mode="markers",
+
+            # Label each dot as their respective pitch type
             name=pitch,
+
+            # Everything here is identical to previous plots
             marker=dict(size=8, color=colors.get(pitch, "black"), line=dict(color="white", width=0.5)),
             customdata=group.index.tolist(),
         ))
 
     fig4.update_layout(
+
+        # Add titles
         title="Pitch Location Chart",
         xaxis_title="Plate Location Side (ft)",
         yaxis_title="Plate Location Height (ft)",
+
+        # Set size
         width=550,
         height=550,
+
+        # Set axes
         xaxis=dict(range=[-3, 3], showgrid=False, zeroline=False, zerolinecolor="black", zerolinewidth=2),
         yaxis=dict(range=[-1, 6], showgrid=False, zeroline=False, zerolinecolor="black", zerolinewidth=2),
         plot_bgcolor="white",
@@ -207,6 +293,7 @@ with c3:
         ]
     )
 
+    # Add lines to visualize strike zone on the plot
     fig4.add_shape(type="line", x0=-0.75, x1=-0.75, y0=1.65, y1=3.65, line=dict(color="black", width=2))
     fig4.add_shape(type="line", x0=0.75, x1=0.75, y0=1.65, y1=3.65, line=dict(color="black", width=2))
     fig4.add_shape(type="line", x0=-0.25, x1=-0.25, y0=1.65, y1=3.65, line=dict(color="black", width=1))
@@ -216,15 +303,18 @@ with c3:
     fig4.add_shape(type="line", x0=-0.75, x1=0.75, y0=2.32, y1=2.32, line=dict(color="black", width=1))
     fig4.add_shape(type="line", x0=-0.75, x1=0.75, y0=2.99, y1=2.99, line=dict(color="black", width=1))
 
+    # Display plot with key "loc_plot"
     event4 = st.plotly_chart(fig4, on_select="rerun", key="loc_plot")
 
+    # When pitches are selected
     if event4 and event4.selection and event4.selection.points:
         selected_indices = [int(pt["customdata"]) for pt in event4.selection.points]
         
         st.markdown(f"**{len(selected_indices)} pitches selected**")
         
         new_type = st.selectbox("Reclassify all selected as:", pitch_types)
-        
+
+        # Give users the option between reassigning pitch types or deleting selected pitches
         col1, col2 = st.columns(2)
         with col1:
             if st.button("Apply Change"):
